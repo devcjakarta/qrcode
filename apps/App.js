ddefe3bcd4235c22edc01df0f8b726babbf1344c
administrator <aisymuhammad@gmail.com>
2017-10-26 20:05:35 +0700

    first upload

---
+++ apps/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Android
+} from 'react-native';
+
+import QRcode from 'react-native-qrcode-scanner';
+
+const instructions = Platform.select({
+  // ios: 'Press Cmd+R to reload,\n' +
+  //   'Cmd+D or shake for dev menu',
+  // android: 'Double tap R on your keyboard to reload,\n' +
+  //   'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component {
+  onSuccess(e){
+    Linking.openURL(e.data).catch(err => console.error('An error occured', err));
+  }
+
+  render() {
+    return (
+      
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ index.js
@@ -0,0 +1,6 @@
+import { AppRegistry } from 'react-native';
+import App from './apps/App';
+
+
+
+AppRegistry.registerComponent('qrcode', () => App);
